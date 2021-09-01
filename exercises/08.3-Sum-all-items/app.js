09286e5d402c7c986f85e2e10c9e7651237bbe4a
George Mihov <gmihov001@gmail.com>
2021-09-01 18:12:21 +0000

    Solved 11

--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -3,6 +3,12 @@ function sumTheElements(theArray){
 	var total = 0;
 	
 	//your code here
+	for (let index = 0; index < theArray.length; index++) {
+		const element = theArray[index];
+		total += element;
+	}
 
 	return total;
 }
+
+console.log(sumTheElements([900000, 25000, 960]));
--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -1,7 +1,11 @@
 let i = 20;
+
 do {
     // Magic goes here;
-    
+    if(i % 5 == 0) console.log(i + "!");
+    else console.log(i);
     i--;
+    if(i == 0) console.log('LIFTOFF');
 } while (i > 0);
-console.log('LIFTOFF')
+
+
